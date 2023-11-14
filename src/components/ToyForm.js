956d581cd5369447a18303f5818a83cc4acaa05e
clairemuiru1 <muiruclaire@gmail.com>
2023-11-14 07:34:49 +0300

    "POST request"

--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,15 +1,50 @@
-import React from "react";
+import React ,{useState} from "react";
 
 function ToyForm() {
+  const [formData,newFormData]=useState({
+    name:"",
+    image:""
+  })
+
+  const handleInputChange = (e) =>{
+    const {name, value} = e.target
+    newFormData({
+      ...formData,
+      [name] : value
+    })
+  }
+
+  async function newToy(e) {
+    e.preventDefault()
+    try{
+      const response = await fetch ("http://localhost:3001/toys",{
+        method : "POST",
+        headers: { "Content-Type": "application/json"},
+        body:JSON.stringify(formData)
+      })
+      if(response.ok){
+        newFormData({
+          name:'',
+          image:''
+        })
+      }else{
+        console.log("Failed to create toy")
+      }
+
+    } catch (error){
+      console.log("Error occured while creating toy:", error)
+    }
+  }
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={newToy}>
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+          onChange={handleInputChange}
         />
         <br />
         <input
@@ -17,14 +52,10 @@ function ToyForm() {
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          onChange={handleInputChange}
         />
         <br />
-        <input
-          type="submit"
-          name="submit"
-          value="Create New Toy"
-          className="submit"
-        />
+        <button>Create new toy</button>
       </form>
     </div>
   );
